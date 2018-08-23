631cd0b1f27fbe6894d9541465876cb3c8437482
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2018-08-22 22:01:08 -0700

    switch to GPLv3

--- oauth/oauth1.scm
+++ oauth/oauth1.scm
@@ -4,22 +4,18 @@
 ;;
 ;; This file is part of guile-oauth.
 ;;
-;; guile-oauth is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License as
-;; published by the Free Software Foundation; either version 3 of
-;; the License, or (at your option) any later version.
+;; guile-oauth is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3 of the License, or
+;; (at your option) any later version.
 ;;
-;; guile-oauth is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
+;; guile-oauth is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;; General Public License for more details.
 ;;
 ;; You should have received a copy of the GNU General Public License
-;; along with this program; if not, contact:
-;;
-;; Free Software Foundation           Voice:  +1-617-542-5942
-;; 59 Temple Place - Suite 330        Fax:    +1-617-542-2652
-;; Boston, MA  02111-1307,  USA       gnu@gnu.org
+;; along with guile-oauth. If not, see https://www.gnu.org/licenses/.
 
 ;;; Commentary:
 
